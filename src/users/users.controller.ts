c2e3206c4989a9e714545795f33f8b80c7369b63
Nadiia-Valkiv-NerdySoft <nadiia.valkiv@nerdysoft.com>
2026-02-02 22:40:21 +0200

    fixed comments after review

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+  Res,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import express from 'express';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  getAllUsers(@Res() res: express.Response) {
+    res.status(HttpStatus.OK).send(this.usersService.findAll());
+  }
+
+  @Get(':id')
+  getUserById(@Param('id', ParseIntPipe) id: number, @Res() res: express.Response) {
+    res.status(HttpStatus.OK).send(this.usersService.findOne(id));
+  }
+
+  @Post()
+  createUser(@Body() createUserDto: CreateUserDto, @Res() res: express.Response) {
+    res
+      .status(HttpStatus.CREATED)
+      .send(this.usersService.create(createUserDto));
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import type { User } from './interfaces/user.interface';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users: User[] = [
+    { id: 1, name: 'Nadia', email: 'nadia@example.com' },
+    { id: 2, name: 'Maria', email: 'maria@example.com' },
+  ];
+  private currentId = 3;
+
+  findAll(): User[] {
+    return this.users;
+  }
+
+  findOne(id: number): User {
+    const user = this.users.find(user => user.id === id);
+    if (!user) {
+      throw new NotFoundException(`Користувача з ID ${id} не знайдено`);
+    }
+    return user;
+  }
+
+  create(createUserDto: CreateUserDto): User {
+    const newUser: User = {
+      id: this.currentId++,
+      name: createUserDto.name,
+      email: createUserDto.email,
+    };
+    this.users.push(newUser);
+    return newUser;
+  }
+}
