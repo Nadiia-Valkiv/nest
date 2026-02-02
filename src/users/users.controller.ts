971ddb6db289a977b3d17a7399e24a6534436435
Nadiia-Valkiv-NerdySoft <nadiia.valkiv@nerdysoft.com>
2026-02-02 22:57:15 +0200

    update controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,34 +2,33 @@ import {
   Body,
   Controller,
   Get,
+  HttpCode,
   HttpStatus,
   Param,
   ParseIntPipe,
   Post,
-  Res,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import express from 'express';
+import type { User } from './interfaces/user.interface';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
-  getAllUsers(@Res() res: express.Response) {
-    res.status(HttpStatus.OK).send(this.usersService.findAll());
+  getAllUsers(): User[] {
+    return this.usersService.findAll();
   }
 
   @Get(':id')
-  getUserById(@Param('id', ParseIntPipe) id: number, @Res() res: express.Response) {
-    res.status(HttpStatus.OK).send(this.usersService.findOne(id));
+  getUserById(@Param('id', ParseIntPipe) id: number): User {
+    return this.usersService.findOne(id);
   }
 
   @Post()
-  createUser(@Body() createUserDto: CreateUserDto, @Res() res: express.Response) {
-    res
-      .status(HttpStatus.CREATED)
-      .send(this.usersService.create(createUserDto));
+  @HttpCode(HttpStatus.CREATED)
+  createUser(@Body() createUserDto: CreateUserDto): User {
+    return this.usersService.create(createUserDto);
   }
 }
